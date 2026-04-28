ffcbf4eb315f150a9d3ab34d5ed376d81ce8773a
José Jorge Melo <josejorgehmdemelo@gmail.com>
2026-04-28 19:26:46 -0300

    Criação do main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from google import genai
+
+app = Flask(__name__)
+
+# Configure sua permissão
+client = genai.Client(api_key='SUA_CHAVE_API')
+
+# Lista para guardar o histórico (zera se reiniciar o servidor)
+historico_chat = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pergunta = request.form.get('pergunta')
+        
+        # Adiciona sua pergunta ao histórico
+        historico_chat.append({"role": "user", "text": pergunta})
+
+        # Chama a API do Gemini
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=(f"{pergunta} - Responda em português.")
+        )
+        
+        # Adiciona a resposta da IA ao histórico
+        historico_chat.append({"role": "bot", "text": response.text})
+
+    return render_template('index.html', chat=historico_chat)
+
+if __name__ == '__main__':
+    app.run(debug=True)
